616af413279323effaf4eaabe1c3d7c482a7d3f4
naldal <cec7378@gmail.com>
2020-09-14 16:37:18 +0900

    algor

--- This is Coding Test/base.py
+++ This is Coding Test/base.py
@@ -1,21 +1,26 @@
-n = int(input())
-array = []
-for _ in range(n-1):
-    array.append(int(input()))
+pad = []
+for i in range(10):
+    pad.append(list(map(int, input().split())))
 
-array.sort()
-flag = False
+x, y = 1, 1
 
-for x in range(n-2):
-    expect = array[x]+1
-    if array[x+1] != expect:
-        print(expect)
-        flag = True
-        continue
-    elif array[0] != 1:
-        print("1")
+while True:
+    if pad[x][y] == 2:  # 먹이 발견했을때 break
+        pad[x][y] = 9
         break
-    elif x == n-3 and flag is False:
-        print(array[n-2]+1)
+    elif pad[x + 1][y] == 1 and pad[x][y + 1] == 1:  # 벽으로 가로 막혔을때 break
+        pad[x][y] = 9
+        break
+    pad[x][y] = 9
+    if pad[x][y + 1] == 1:  # 오른쪽이 벽이면 아래로 1칸
+        x += 1
+    elif pad[x + 1][y] == 1:  # 아래쪽이 벽이면 오른쪽으로 1칸
+        y += 1
+    else:
+        y += 1  # 주변에 벽이 없으면 오른쪽으로 1칸
+pad[x][y] = 9
 
-
+for i in pad:
+    for j in i:
+        print(j, end=' ')
+    print()
