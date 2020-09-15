dda052bad4b26cd9681739bac6fec1ea4145a652
naldal <cec7378@gmail.com>
2020-09-15 15:23:04 +0900

    algor

--- This is Coding Test/base.py
+++ This is Coding Test/base.py
@@ -1,26 +1,12 @@
-pad = []
-for i in range(10):
-    pad.append(list(map(int, input().split())))
+n = int(input())
+sosu = []
+flag = False
+for i in range(2, n):
+    for x in range(1, i+1):
+        if i % x == 0 and (x == 1 or x == i):
+            flag = True
+    if flag is True :
+        sosu.append(x)
+        flag = False
 
-x, y = 1, 1
 
-while True:
-    if pad[x][y] == 2:  # 먹이 발견했을때 break
-        pad[x][y] = 9
-        break
-    elif pad[x + 1][y] == 1 and pad[x][y + 1] == 1:  # 벽으로 가로 막혔을때 break
-        pad[x][y] = 9
-        break
-    pad[x][y] = 9
-    if pad[x][y + 1] == 1:  # 오른쪽이 벽이면 아래로 1칸
-        x += 1
-    elif pad[x + 1][y] == 1:  # 아래쪽이 벽이면 오른쪽으로 1칸
-        y += 1
-    else:
-        y += 1  # 주변에 벽이 없으면 오른쪽으로 1칸
-pad[x][y] = 9
-
-for i in pad:
-    for j in i:
-        print(j, end=' ')
-    print()
