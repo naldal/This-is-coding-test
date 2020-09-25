20fe0b1333c20050d2ea816d4b3bb2437b6cc806
naldal <cec7378@gmail.com>
2020-09-25 23:19:46 +0900

    algor

--- This is Coding Test/pivo.py
+++ This is Coding Test/pivo.py
@@ -1,21 +1,12 @@
-n = int(input())
-arr = [0] * 201
-arr[0] = 1
-arr[1] = 1
-i = 2
+def solution(a, b):
+    if a > b:
+        a, b = b, a
+    result = 0
+    while a <= b:
+        result += a
+        a += 1
+
+    return result
 
 
-def recur(n, i):
-    if n == 1 or n == 2:
-        print(arr[n-1] % 10009)
-        return
 
-    arr[i] += arr[i - 1] + arr[i - 2]
-
-    if n == i:
-        print(arr[n - 1] % 10009)
-        return
-    recur(n, i + 1)
-
-
-recur(n, i)
